7fcdde0b61862805daeceac8dc842b4c74c7e383
R-ronivaldo <48776467+R-ronivaldo@users.noreply.github.com>
2019-04-08 21:38:26 -0300

    Update Kindle.cpp

--- Kindle.cpp
+++ Kindle.cpp
@@ -4,25 +4,31 @@
 using std::cout;
 using std::endl;
 
-Kindle::Kindle(const string &y){
-    setNomeLivro(y);
+Kindle::Kindle(const string &y1,int y2,const string &y3,int y4){
+    setLivro(&y1,y2,&y3,y4);
 }
 
 Kindle::Kindle(){
-    setNomeLivro();
+    setLivro();
 }
    
-void Kindle::setNomeLivro(const string &x){
-    nomeLivro = x;
-    getNomeLivro(nomeLivro);
+void Kindle::setLivro(const string &x1,int x2,const string &x3,int x4){
+    nomeLivro = &x1;
+    idLivro = x2;
+    autorLivro = &x3;
+    anoLivro = x4;
+    getLivro(nomeLivro,idLivro,autorLivro,anoLivro);
 }
 
-void Kindle::setNomeLivro (){
+void Kindle::setLivro () const{
     cin << "Nenhum Nome de livro foi setado" << endl;
 }
 
-void getNomeLivro(const string &j){
-    cin << j << endl;
+void getLivro(const string &j1,int j2,const string &j3,int j4) const{
+    cin << "Nome do Livro: " << &j1 << endl;
+    cin << "Id do Livro: " << j2 << endl;
+    cin << "Autor do Livro: " << &j3 << endl;
+    cin << "Ano do Livro: " << j4 << endl;
 }
 
 Kindle::~Kindle()
