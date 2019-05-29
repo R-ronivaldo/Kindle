876776d2ad79e82bd32c6a83efb50d3613846c2d
R-ronivaldo <48776467+R-ronivaldo@users.noreply.github.com>
2019-05-29 05:28:18 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -18,16 +18,16 @@ int main(int argc, char** argv) {
 	cout << "Insira o nome do Usuario :";
 	cin >> objL;
 	Kindle kindle1 (objL);
-	kindle1.imprimirUser();
+	cout << kindle1;
 	
 	
 	cout << "Insira o nome da Biblioteca :";
 	cin >> objB;
-	Biblioteca biblio1("teste");
+	Biblioteca biblio1(objB);
 	kindle1.adicionarBiblioteca(biblio1);
-	system("cls");
+	cout << biblio1;
 			
-	Livro livro1("HarryPotter","J.K","Fantasia1",2010);
+	Livro livro1("HarryPotter1","J.K","Fantasia1",2010);
 	Livro livro2("HarryPotter2","J.K2","Fantasia2",2011);
 	Livro livro3("HarryPotter3","J.K3","Fantasia3",2010);
 	//Livro livro4("HarryPotter4","J.K4","Fantasia4",2013);
@@ -37,7 +37,7 @@ int main(int argc, char** argv) {
 	Livro livro7("HarryPotter7","J.K7","Fantasia7",2015);
 	Livro livro8("HarryPotter8","J.K8","Fantasia8",2009);
 	Livro livro9("HarryPotter9","J.K9","Fantasia9",2010);
-	Livro livro10("HarryPotter13","J.K13","Fantasia13",2015);
+	Livro livro10("HarryPotter10","J.K13","Fantasia13",2015);
 
 	while (opcao != 0){
 		system("cls");
@@ -138,22 +138,21 @@ int main(int argc, char** argv) {
 	
 	}
 	
+	cout << endl;
+	cout << "Parte apenas para testes!" << endl;
 	
-	/*Biblioteca biblio2;
-	biblio2 += biblio1;
+	Biblioteca biblio2("teste");
+	cout << biblio2;
 
 	biblio2.adicionarLivro(livro1);
 	biblio2.adicionarLivro(livro4);
 	biblio2.imprimeBiblio();
-	
 	
 	if (biblio1 < biblio2){
 		cout << "Biblioteca 2 e maior";
 	} else {
 		cout << "Biblioteca 1 e maior";
 	}
-	
-	*/
 	
 	if (livro1 < livro2){
 		cout << endl;
